87bde28ce8bd92490b2d10652c06e62be64167b1
goga574 <discordforgoa@gmail.com>
2024-11-28 16:59:29 +0400

    1

---
+++ level 16/homework/hw5.py
@@ -0,0 +1,10 @@
+#   5) დაბეჭდეთ 5 დან 30 მდე ყველა ლუწი რიცხვი while loop ის და if else გამოყენებით
+
+i = 5
+
+while i < 30:
+    if i % 2==0:
+        print(i)
+    else:
+        print(str(i) + "odd")
+    i+=1
