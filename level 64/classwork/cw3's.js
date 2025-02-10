97ad607d3d6a48149641c8dc073401081cf0570c
goga574 <discordforgoa@gmail.com>
2025-02-10 20:54:10 +0400

    1

---
+++ level 64/classwork/cw3's.js
@@ -0,0 +1,8 @@
+
+var num1 = Number(prompt("Enter First Number"))
+var num2 = Number(prompt("Enter Second Number"))
+
+console.log(num1 + num2)
+console.log(num1-num2)
+console.log(num1/num2)
+console.log(num1*num2)
---
+++ level 64/classwork/cw4's.js
@@ -0,0 +1,4 @@
+var name1 = prompt("Enter Your Name")
+
+
+alert("Hello" + " " + name1)
