bad6e9e5b6877f822d67329870b87ee9f249fe4c
goga574 <discordforgoa@gmail.com>
2025-03-02 13:03:01 +0400

    1

--- level 71/homework/hw4.js
+++ level 71/homework/hw4.js
@@ -1,11 +1,11 @@
 //  შექმენით სია სადაც იქნება user-ების სახელი და გვარი და ახალ სიაში დაამატეთ ამ იუსერების  ინიციალები სახელის და გვარის პირველი ასოები, მაგალითად: გიორგი ბიბილაშვილი -> გ.ბ 
 
 
-let arr = [["giorgi","giorgobiani"],["beqa","gviniashvili"],["lasha","sirbiladze"]]
+// let arr = [["giorgi","giorgobiani"],["beqa","gviniashvili"],["lasha","sirbiladze"]]
 
-let empty = []
+// let empty = []
 
-for(let i of arr){
-    empty.push(arr[i][0])
-}
-console.log(empty)
+// for(let i of arr){
+//     empty.push(arr[i][0])
+// }
+// console.log(empty)
