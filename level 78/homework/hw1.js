964ce891a083e207ad3104e8c00f83d5385b29e0
goga574 <discordforgoa@gmail.com>
2025-03-30 01:59:53 +0400

    1

--- level 78/homework/hw1.js
+++ level 78/homework/hw1.js
@@ -1,23 +1,53 @@
-// let img = document.getElementById("img")
-// let next = document.getElementById("nextpic")
-// let prev = document.getElementById("previous")
+let img = document.getElementById("img")
+let next = document.getElementById("nextpic")
+let prev = document.getElementById("previous")
 
-let x = 0
+// let x = 0
 
-images = ["first.png","second.png","third.png"]
+// images = ["first.png","second.png","third.png"]
 
-nextpic.addEventListener("click",function(){
-    x++
-    if(x>images.length-1){
-        x = 0
+// nextpic.addEventListener("click",function(){
+//     x++
+//     if(x>images.length-1){
+//         x = 0
+//     }
+//     img.src = images[x]
+// })
+
+// previous.addEventListener("click",function(){
+//     x--
+//     if(x<0){
+//         x = images.length - 1
+//     }
+//     img.src = images[x]
+// })
+
+
+
+let images = ["first.png","second.png","third.png"]
+
+let index  = 0
+
+next.addEventListener("click",function(){       
+    if(index == images.length-1){
+        index = 0
+        console.log(index)
+    }else{
+        index+=1
+        console.log(index)
     }
-    img.src = images[x]
+    img.src = images[index]
+    
 })
 
-previous.addEventListener("click",function(){
-    x--
-    if(x<0){
-        x = images.length - 1
+prev.addEventListener("click",function(){
+    if(index == 0){
+        index = images.length-1
+        console.log(index)
+
+    }else{
+        index-=1
+        console.log(index)
     }
-    img.src = images[x]
+    img.src = images[index]
 })
