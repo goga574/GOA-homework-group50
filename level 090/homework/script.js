5855170fa618c6ae00f41a0d8aa38df37c0ecb00
goga574 <discordforgoa@gmail.com>
2025-05-15 16:50:07 +0400

    1

---
+++ level 090/homework/script.js
@@ -0,0 +1,75 @@
+let showsidebar = document.querySelector(".showsidebar")
+
+let sidebar = document.querySelector(".sidebar")
+
+let quit = document.querySelector(".quit")
+
+showsidebar.addEventListener("click",()=>{
+    sidebar.style.right = "0px"
+})
+
+quit.addEventListener("click",()=>{
+     sidebar.style.right = "-300px"
+})
+
+
+
+let addButton = document.querySelectorAll(".addbutton")
+
+let card = document.querySelectorAll(".pcdiv")
+
+let count = 0
+
+let priceCounter = 0
+
+let quantity = document.querySelectorAll(".totamount")
+
+let totalPrice = document.querySelectorAll(".totprice")
+
+
+
+for(let i = 0;i<addButton.length;i++){
+    addButton[i].addEventListener("click",()=>{
+        let info = card[i]
+        let img = info.querySelector(".img").src
+        let name = info.querySelector(".pcname").textContent
+        let price = info.querySelector(".pcprice").textContent
+        count++
+        quantity[0].textContent = count
+
+
+        let tot = priceCounter+=Number(price)
+        totalPrice[0].textContent = tot
+
+
+        let newDiv = document.createElement("div")
+        newDiv.classList.add("pc-cart")
+
+        let img1 = document.createElement("img")
+        img1.classList.add("change")
+        img1.src = img
+
+        let childDiv = document.createElement("div")
+        childDiv.classList.add("cart-info")
+
+        let nameP = document.createElement("p")
+        nameP.textContent = name
+
+        let priceP = document.createElement("p")
+        priceP.textContent = `$${price}`
+
+        let removeBut = document.createElement
+        ("button")
+       
+        
+    
+
+        sidebar.appendChild(newDiv)
+        childDiv.appendChild(nameP)
+        childDiv.appendChild(priceP)
+        childDiv.appendChild(removeBut)
+        newDiv.appendChild(img1)
+        newDiv.appendChild(childDiv)
+        
+    })
+}
