7fc90f6a3fb14a3244f1ba6dc782216442804ac1
goga574 <discordforgoa@gmail.com>
2025-09-13 14:16:48 +0400

    1

---
+++ level 128/classwork/script.js
@@ -0,0 +1,18 @@
+
+
+
+// function recieveData(data){
+//     let weather = data
+//     console.log(weather)
+// }
+
+
+// function getWeather(callback){
+//     setTimeout(()=>{
+//         callback("snow")
+//     },1000)
+// }
+
+
+// getWeather(recieveData)
+
