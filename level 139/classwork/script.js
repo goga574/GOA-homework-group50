6709c3d728ace625a0696f97ebe1ea898aef699c
goga574 <discordforgoa@gmail.com>
2025-10-12 23:08:54 +0400

    1

--- level 139/classwork/script.js
+++ level 139/classwork/script.js
@@ -1,3 +1,24 @@
-console.log("hello")
+// import {userName,userSurname} from "/level 139/classwork/script2.js";
 
-let name = "goga"
+// console.log(userName)
+// console.log(userSurname)
+
+
+// =================================
+// import {userName,userSurname} from "/level 139/classwork/script2.js";
+
+// userName.style.backgroundColor = "red"
+
+// userSurname.style.backgroundColor = "blue"
+
+
+// =================================
+
+
+// import { multy } from "/level 139/classwork/script2.js";
+
+// console.log(multy(5,6))
+
+import test from "/level 139/classwork/script2.js";
+
+test()
--- level 139/classwork/script2.js
+++ level 139/classwork/script2.js
@@ -1,3 +1,33 @@
-console.log("goa best")
+// let userName = document.querySelector("#name")
 
-console.log(name + " opp")
+// let userSurname =document.querySelector("#surname")
+
+// export {userName,userSurname}
+
+
+
+// =================================
+
+// function multy(a,b){
+//     return a*b
+// }
+
+// export {multy}
+
+
+
+let input = document.querySelector("input")
+
+let value = "test"
+
+function test() {
+    input.addEventListener("change", (e) => {
+        value = e.target.value
+        console.log(value)
+
+        return value
+    })
+}
+
+
+export default test
