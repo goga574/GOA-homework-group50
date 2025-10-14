2a60fff3781bbf3cc6ea6c44fe8e23717e73cf36
goga574 <discordforgoa@gmail.com>
2025-10-14 15:23:05 +0400

    1

---
+++ level 146/classwork/vite4/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import Card from './Card'
+
+function App() {
+
+
+  return (
+    <>
+      <Card img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYySwAPxkoPUJ_OZCC-bl4dVkjWuwa_cMetg&s" title="Mc laren" button="click me1"/>
+      <Card img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxtUY8F0SRXn0HUYQQYr-6Fa8hPKGEx1yQ4A&s" title="Wolksagen" button="click me2"/>
+
+      <Card img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeyRRoFw7h_1cSZW91g_jKbCOs4e1omKUWpg&s" title="Mercedes" button="click me3"/>
+    </>
+  )
+}
+
+export default App
+
+
+// 
