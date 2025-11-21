cd92517b9084181d27e959506d668ed429077fcb
goga574 <discordforgoa@gmail.com>
2025-11-21 15:50:44 +0400

    1

---
+++ level 149/classwork/my-project/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className='flex -between'>
+        <p className='text-green-500  bg-yellow-500 h-6'>goga</p>
+
+        <p className='text-green-500  bg-yellow-500 h-6'>goga</p>
+      </div>
+    </>
+  )
+}
+
+export default App
